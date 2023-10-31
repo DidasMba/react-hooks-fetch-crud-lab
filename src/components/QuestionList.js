a7806f206d7917b8e122df783b87c3a2be76b4d8
DidasMba <daniel.mbarushi@gmail.com>
2023-10-31 15:44:17 +0300

    add QuestionlIST

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,40 @@
-import React from "react";
+/*import React from "react";
 
 function QuestionList() {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{* display QuestionItem components here after fetching *}</ul>
+    </section>
+  );
+}
+
+export default QuestionList;*/
+
+
+import React, { useState, useEffect } from "react";
+
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Make a GET request to fetch questions from the API
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  return (
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}
+      </ul>
     </section>
   );
 }
 
 export default QuestionList;
+
